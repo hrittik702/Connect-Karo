f8761646c60402a4aeec582ec152a2694dd18065
hrittik702 <mmhrittik@gmail.com>
2026-05-28 19:16:06 +0530

    fix : College Details Connect with Supase College

--- src/pages/admin/colleges/AddCollege.jsx
+++ src/pages/admin/colleges/AddCollege.jsx
@@ -158,7 +158,7 @@ export default function AddCollege() {
 
       // 7. REAL AUTHENTICATION INJECTION (Secondary Supabase Client)
       const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-      const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+      const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
       const secondarySupabase = createClient(supabaseUrl, supabaseAnonKey, {
         auth: {
